eda8c2e16028d5c38a5953c2abfcdec04d3c73fd
Salina <amargunjal28@gmail.com>
2020-01-29 15:47:46 +0530

    AADED:Employee Attendance

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "Welcome"
+echo "Welcome to Employee Wage Computation Program"
+
+ISPRESENT=1
+rand=$(($RANDOM%2))
+
+if(($rand==1))
+then
+   echo "Employee is present"
+else
+   echo "Employee is absent"
+fi
